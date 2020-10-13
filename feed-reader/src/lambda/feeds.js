3f20080ee2d948ef17d80110ac1d56f9bb973ced
William <willteey@gmail.com>
2020-10-13 07:13:26 +0100

    Quiz Up Js

--- feed-reader/src/lambda/feeds.js
+++ feed-reader/src/lambda/feeds.js
@@ -1,22 +1,22 @@
-const Parser = require("rss-parser");
+const Parser = require('rss-parser')
 
 export function handler(event, context, callback) {
   const FEEDS = [
-    "https://codepen.io/posts/feed",
-    "https://hnrss.org/frontpage",
-    "https://github-trends.ryotarai.info/rss/github_trends_javascript_daily.rss",
-  ];
+    'https://codepen.io/posts/feed',
+    'https://hnrss.org/frontpage',
+    'https://github-trends.ryotarai/rss/github_trends_javascript_daily.rss'
+  ]
 
-  let parser = new Parser();
+  let parser = new Parser()
+  const feedRequest = FEEDS.map(feed => {
+    return parser.parseURL(feed)
 
-  const feedRequests = FEEDS.map((feed) => {
-    return parser.parseURL(feed);
-  });
+  })
 
-  Promise.all(feedRequests).then((response) => {
+  Promise.all(feedRequest).then(response => {
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify(response),
-    });
-  });
+      body: JSON.stringify(response)
+    })
+  })
 }
